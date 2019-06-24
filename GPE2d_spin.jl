bc17817b5a8c687a590a7d9be1e2db1501b50dbc
Kyle Bomeisl <45512800+bomeisl@users.noreply.github.com>
2019-06-24 07:41:02 -0400

    Optimization and fftshift, energy function

--- GPE2d_spin.jl
+++ GPE2d_spin.jl
@@ -3,43 +3,32 @@
 #input parameters--------------------------
 
 #size of step in real time
-delt_t = (10^-3)
+del_t = (40^-1)
 
 fib(n) = n < 2 ? n : fib(n-1) + fib(n-2)
 
 #range over real space (-x_end:x_end)
-n = 12
-N_x = fib(n)
-N_y = fib(n)
-
-#x and y real space array generator
-x_array = collect(1:fib(n))
-y_array = collect(1:fib(n))
-
+n = fib(12)
+m = fib(10)
 #V HO "spring constant"
-m = 1
-omega = 0
+# const m = 1
+# const omega = 0
 #strength of particle-particle interactions (V coupling constant)
-g = 0
+# const g = 0
 # Energy scale for Pot QP
-W = 1
+# W = .2
 #phase for Pot QP
-phi_x = 0
-phi_y = 0
-#spin-orbital coupling constant
-s = 1
+
 #momentum kinetic energy strength
-t = 0
+# const t = 0
 #number of particles
-N = 1
-#magnitude of test Gaussian
-A = 1
+# const N = 1
 #---------------------------------------------------
 
 #----Enter the starting parameters here
 # Write the guess wavefunction here
 function psi_guess(x,y,z)
-    return A*exp(-(x^2) - (y^2))
+    return exp(-((x-73)^2) - ((y-73)^2))
 end
 
 #write the potential energy here
@@ -49,8 +38,11 @@ end
 
 #Write the Quasi-Periodic Potential Energy here
 function pot_QP(x,y)
-    Q = (2/pi)*(fib(n-2)/fib(n))
-    return pot_QP = W*(cos((Q*x)+phi_x) + cos((Q*y)+phi_y))
+    W = 1
+    phi_x = 0
+    phi_y = 0
+    Q = (2/pi)*(m/n)
+    return W*(cos((Q*x)+phi_x) + cos((Q*y)+phi_y))
 end
 
 
@@ -59,10 +51,10 @@ end
 using LinearAlgebra, Statistics, Compat
 #generates the array that is the discretization of the guess function in real space
 function psi_guess_array()
-    psi_guess_array = zeros(ComplexF64, N_x, N_y, 2)
+    psi_guess_array = zeros(ComplexF64, n, n, 2)
 
-    for x in 1:N_x
-        for y in 1:N_y
+    for x in 1:n
+        for y in 1:n
             psi_guess_array[x,y,1] = psi_guess(x,y,-1)
             psi_guess_array[x,y,2] = psi_guess(x,y,1)
         end
@@ -74,35 +66,37 @@ end
 #array[:,:,1].* conj(array[:,:,1])) + (array[:,:,2].*conj(array[:,:,2])
 function normalizer(array)
     s = sqrt(sum(array.*conj(array)))
-    return (array/s).*sqrt(N)
+    return (array/s)
 end
 
 #-----------------POTENTIAL ENERGY
 
 #Generates the potential energy array
 function pot_array_H_O(psi)
-    pot_array = zeros(ComplexF64, N_x, N_y)
+    pot_array = zeros(ComplexF64, n, n)
 
-    for n in 1:N_x
-        for m in 1:N_y
-            pot_array[n,m] = pot_H_O(x_array[n], y_array[m])
+    for i in 1:n
+        for j in 1:n
+            pot_array[i,j] = pot_H_O(x_array[i], y_array[j])
         end
     end
 
     psi = psi[:,:,1]+psi[:,:,2]
-    psi = reshape(psi, N_x, N_y)
+    psi = reshape(psi, n, n)
 
     pot_array = pot_array+g*(conj(psi).*psi)
     return pot_array
 
 end
 
-function pot_array_QP()
-    pot_array = zeros(ComplexF64, N_x, N_y)
 
-    for n in 1:N_x
-        for m in 1:N_y
-            pot_array[n,m] = pot_QP(x_array[n], y_array[m])
+
+function pot_array_QP()
+    pot_array = zeros(ComplexF64, n, n)
+
+    for i in 1:n
+        for j in 1:n
+            pot_array[i,j] = pot_QP(i,j)
         end
     end
 
@@ -110,16 +104,18 @@ function pot_array_QP()
 
 end
 
+pot_matrix_QP = pot_array_QP()
+
 #---------------KINETIC ENERGY
 
 #calculates the x momentum for the fft minted momentum eignestate
 function p_x(n)
-    return p_x = ((2*pi)*n-1)/N_x
+    return ((2*pi)*(n-1))/n
 end
 
 #calculates the y momentum for the fft minted momentum eignestate
 function p_y(m)
-    return p_y = ((2*pi)*m-1)/N_y
+    return ((2*pi)*(m-1))/n
 end
 
 #calculates the total kinetic energy for each fft minted momentum eigenstate
@@ -130,55 +126,61 @@ end
 
 #Generates the momentum Kinetic Energy array
 function kin_mom_array()
-    kin_array = zeros(ComplexF64, N_x, N_y)
+    kin_array = zeros(ComplexF64, n, n)
 
-    for n in 1:N_x
-        for m in 1:N_y
-            kin_array[n,m] = kin_mom(p_x(n), p_y(m))
+    for i in 1:n
+        for j in 1:n
+            kin_array[n,m] = kin_mom(p_x(i), p_y(j))
         end
     end
     return kin_array
 end
 
 #generates the spin orbital coupling matrix
-function kin_spin_matrix(x, y)
-    spin_couple_matrix = zeros(ComplexF64,2,2)
-    A = sin(p_x(x)) - im*sin(p_y(y))
-    B = sin(p_x(x)) + im*sin(p_y(y))
-    spin_couple_matrix[1,1] = s*cos(sqrt(A*B)*delt_t/2)
-    spin_couple_matrix[1,2] = -s*im*sqrt(A)/sqrt(B)*sin(sqrt(A*B)*delt_t/2)
-    spin_couple_matrix[2,1] = -s*im*sqrt(B)/sqrt(A)*sin(sqrt(A*B)*delt_t/2)
-    spin_couple_matrix[2,2] = s*cos(sqrt(A*B)*delt_t/2)
+
+
+function kin_spin_matrix()
+    spin_couple_matrix = zeros(ComplexF64, n, n, 2, 2)
+    A(x,y) = sin(p_x(x)) - im*sin(p_y(y))
+    for x in 1:n
+        for y in 1:n
+            spin_couple_matrix[x,y,1,1] = cos(abs(A(x,y)) * del_t/2)
+            spin_couple_matrix[x,y,1,2] = -im*exp(im * angle(A(x,y))) * sin(abs(A(x,y)) * del_t/2)
+            spin_couple_matrix[x,y,2,1] = -im*exp(-im * angle(A(x,y))) * sin(abs(A(x,y)) * del_t/2)
+            spin_couple_matrix[x,y,2,2] = cos(abs(A(x,y)) * del_t/2)
+        end
+    end
     return spin_couple_matrix
 end
 
+spin_matrix = kin_spin_matrix()
+
 #exponentiates the harmonic oscilator pot energy operator elementwise
 function e_V(psi)
-    return exp.(((-m*(omega)^2)/2)*pot_array_H_O(psi).*((-delt_t)*im))
+    return exp.(((-m*(omega)^2)/2)*pot_array_H_O(psi).*((-del_t)*im))
 end
 
 #exponentiates the momentum kin energy operator elementwise
 function e_T()
-    return e_T = exp.(-t*kin_mom_array()*(delt_t/2)*im)
+    return e_T = exp.(-t*kin_mom_array()*(del_t/2)*im)
 end
 
 #evolves psi delt_t in time with the KE operator
+#evaluate this one time and store in memory
+gen_array = zeros(ComplexF64, n, n, 2)
 function time_step_T(array)
-    array3 = zeros(ComplexF64, N_x, N_y, 2)
-    for x in 1:N_x
-        for y in 1:N_y
-            array3[x,y,1] = kin_spin_matrix(x,y)[1,1]*array[x,y,1] + kin_spin_matrix(x,y)[1,2]*array[x,y,2]
-            array3[x,y,2] = kin_spin_matrix(x,y)[2,1]*array[x,y,1] + kin_spin_matrix(x,y)[2,2]*array[x,y,2]
+    for x in 1:n
+        for y in 1:n
+            gen_array[x,y,1] = spin_matrix[x,y,1,1]*array[x,y,1] + spin_matrix[x,y,1,2]*array[x,y,2]
+            gen_array[x,y,2] = spin_matrix[x,y,2,1]*array[x,y,1] + spin_matrix[x,y,2,2]*array[x,y,2]
         end
     end
-    return array3
+    return gen_array
 end
 
 #evolves psi delt_t in time with the PE operator
 function time_step_V(array)
-    array[:,:,1] = array[:,:,1].*(exp.(pot_array_QP()*(-im*delt_t)))
-    array[:,:,2] = array[:,:,2].*(exp.(pot_array_QP()*(-im*delt_t)))
-    return array
+    return array.*(exp.(pot_matrix_QP*(-im*del_t)))
 end
 
 #evolves the guess function array one step in imaginary time
@@ -186,31 +188,28 @@ using FFTW
 using AbstractFFTs
 
 function init_FFT()
-    A = zeros(ComplexF64, N_x,N_y,2)
+    A = zeros(ComplexF64, n, n, 2)
     region = 1:2
-    F_T = plan_fft(A,region; flags=FFTW.PATIENT, timelimit=Inf)
-    return F_T
+    return plan_fft(A,region; flags=FFTW.PATIENT, timelimit=Inf)
 end
 
 function init_IFFT()
-    A = zeros(ComplexF64, N_x,N_y,2)
+    A = zeros(ComplexF64, n, n, 2)
     region = 1:2
-    I_F_T = plan_ifft(A,region; flags=FFTW.PATIENT, timelimit=Inf)
-    return I_F_T
+    return plan_ifft(A,region; flags=FFTW.PATIENT, timelimit=Inf)
 end
 
 F_T = init_FFT()
 I_F_T = init_IFFT()
 
 function time_evolve_step(array)
-    x = F_T*array
+    x = fftshift(F_T*array)
     x = time_step_T(x)
     x = I_F_T*x
     x = time_step_V(x)
-    x = F_T*x
+    x = fftshift(F_T*x)
     x = time_step_T(x)
-    x = I_F_T*x
-    return x
+    return I_F_T*x
 end
 
 #evolves the guess function array t steps in imaginary time
@@ -219,14 +218,38 @@ function time_evolve(array, t)
     return evolved_array
 end
 
+function time_evolve_fast(array,t)
+
+    n_array = [array]
+
+    for i in 2:t
+        push!(n_array, time_evolve_step(n_array[i-1]))
+    end
+    return n_array
+end
+
+function T(x,y)
+    return [[0 sin(p_x(x)) - (im* sin(p_y(y)))]; [sin(p_x(x)) + im * sin(p_y(y)) 0]]
+end
+
+function T_step(array)
+    gen_array = zeros(ComplexF64, n, n, 2)
+    for x in 1:n
+        for y in 1:n
+            gen_array[x,y,:] = T(x,y)*array[x,y,:]
+        end
+    end
+    return gen_array
+end
+
 #Finds the energy of psi
-# function Energy(array)
-#     ffts = fftshift(F_T*array)
-#     kin = I_F_T*(ffts.*kin_array())
-#     pot = psi.*pot_array_H_O(psi)
-#     energy = (kin.+pot).*conj(psi)
-#     return sum(energy)
-# end
+function Energy(array)
+    ffts = F_T*array
+    kin = dot(array, I_F_T*T_step(ffts))
+    pot = dot(array, (pot_matrix_QP.*array))
+    energy = kin + pot
+    return kin
+end
 
 #spread auxillary functions
 function r(x,y)
@@ -238,9 +261,9 @@ function r_2(x,y)
 end
 
 function r_array()
-    r_array = zeros(N_x, N_y)
-    for x in 1:N_x
-        for y in 1:N_y
+    r_array = zeros(n, n)
+    for x in 1:n
+        for y in 1:n
             r_array[x,y] = r(x,y)
         end
     end
@@ -249,9 +272,9 @@ function r_array()
 end
 
 function r_2_array()
-    r_2_array = zeros(N_x, N_y)
-    for x in 1:N_x
-        for y in 1:N_y
+    r_2_array = zeros(n, n)
+    for x in 1:n
+        for y in 1:n
             r_2_array[x,y] = r_2(x,y)
         end
     end
@@ -259,28 +282,28 @@ function r_2_array()
     return r_2_array
 end
 
+r_matrix = r_array()
+r_2_matrix = r_2_array()
+
 #The spread of the wavefunction
 function spread(psi)
 
-    psi_r_up = (conj(psi[:,:,1]).*r_array()).*psi[:,:,1]
-    psi_r_down = (conj(psi[:,:,2]).*r_array()).*psi[:,:,2]
-    psi_r_2_up = (conj(psi[:,:,1]).*r_2_array()).*psi[:,:,1]
-    psi_r_2_down = (conj(psi[:,:,2]).*r_2_array()).*psi[:,:,2]
-    psi_r = psi_r_up + psi_r_down
-    psi_r_2 = psi_r_2_up + psi_r_2_down
+    psi_r = conj(psi).*r_matrix.*psi
+    psi_r_2 = conj(psi).*r_2_matrix.*psi
 
-    return spread = real(sum(psi_r_2) - (sum(psi_r))^2)
+    return real(sum(psi_r_2) - (sum(psi_r))^2)
 
 end
 
 
 #Plotters____________________________________________________
 using Plots
-
-t = 1:50
-psi = psi_guess_array()
-spread_array = zeros(50)
-for t in 1:50
-    spread_array[t] = spread(time_evolve(psi,t))
+function Plotter()
+    t = 1:100
+    psi = psi_guess_array()
+    spread_array = zeros(100)
+    @progress for t in 1:100
+        spread_array[t] = spread(time_evolve_fast(psi,t)[t])
+    end
+    plot!(t, spread_array, xaxis =:log, yaxis =:log, legend = false)
 end
-plot!( spread_array, xaxis=:log, yaxis=:log)
